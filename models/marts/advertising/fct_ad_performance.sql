b25bdf35617bc425603ab2b67b1283b896cc5c60
Alexa Diaz <axela.zaid@gmail.com>
2019-05-09 09:56:42 -0400

    updates to the facebook ads channels

--- models/marts/advertising/fct_ad_performance.sql
+++ models/marts/advertising/fct_ad_performance.sql
@@ -51,14 +51,13 @@ marketing_channels as (
             when lower(campaign) ilike '%display%' then 'display'
             when lower(campaign) ilike '%shopping%'
                 or lower(campaign) ilike '%cpc%' then 'paid search'
-            when utm_medium is null and utm_campaign not in ('fb_retargeted', 'ig_retargeted') 
-                then 'direct'
             when lower(utm_medium) in ('retargeting', 'social', 'influencers') 
                 then utm_medium
             when lower(utm_campaign) in ('fb_retargeted', 'ig_retargeted') 
                 then 'retargeting'
             when lower(utm_campaign) ilike '%amazon%' then 'facebook amazon'
-            when lower(utm_medium) in ('paid', 'paid_social', 'prospecting')
+            when utm_medium is null and platform = 'facebook'
+                or lower(utm_medium) in ('paid', 'paid_social', 'prospecting')
                 then 'facebook ads'
             when lower(utm_medium) in ('paidsearch', 'cpc', 'shopping') 
                 then 'paid search'
