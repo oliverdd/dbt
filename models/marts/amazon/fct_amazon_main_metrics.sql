0867b4235fec410e9ccfbfe70d64353aa5deacec
Faisal Al-Khalidi <faisal@faisals-macbook.attlocal.net>
2019-08-18 19:41:54 -0700

    add amazon order calculations model

--- models/marts/amazon/fct_amazon_main_metrics.sql
+++ models/marts/amazon/fct_amazon_main_metrics.sql
@@ -2,7 +2,7 @@ with skus as (
     select
         coalesce(m.sku,s.sku) as sku,
         coalesce(m.asin,s.asin) as asin
-    from {{ ref('stg_amazon_mws') }} m
+    from {{ ref('stg_amazon_mws_order_items') }} m
     full outer join {{ ref('stg_amazon_sponsored_products') }} s on m.sku = s.sku
 ),
 
